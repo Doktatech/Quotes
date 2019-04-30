45b446c9eb66dab490a3fe7349571c5b85f16db0
Doktatech <Doktatech2@gmail.com>
2019-04-30 10:50:33 +0300

    Edit date count pipe so passed days could be counted

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -8,18 +8,14 @@ export class DateCountPipe implements PipeTransform {
   transform(value: any, args?: any): any {
     let today:Date = new Date(); 
     let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
-    var dateDifference =Math.abs(value-todayWithNoTime )
+    var dateDifference =Math.abs(todayWithNoTime-value )
     const secondsInADay= 86400; 
 
     var dateDifferenceSeconds=dateDifference*0.001; 
 
     var dateCounter = dateDifferenceSeconds/secondsInADay;
 
-    if (dateCounter >= 1 && value > todayWithNoTime){
         return dateCounter;
-    }else{
-        return 0;
 
-}
 }
 }
